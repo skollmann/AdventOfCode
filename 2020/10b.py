f0324f79a8dab977d4b81a291c3985e84772c02b
Stephan Kollmann <stephan.kollmann--git@cl.cam.ac.uk>
2020-12-10 14:22:27 +0100

    10b.py: Rename volts to jolts

--- 2020/10b.py
+++ 2020/10b.py
@@ -1,13 +1,13 @@
 import sys
 
-volts = []
+jolts = []
 for line in sys.stdin:
-    volts.append(int(line.rstrip()))
+    jolts.append(int(line.rstrip()))
 
-volts.sort()
-cnts = [0 for _ in range(volts[-1]+1)]
+jolts.sort()
+cnts = [0 for _ in range(jolts[-1]+1)]
 cnts[0] = 1
-for v in volts:
+for v in jolts:
     cnts[v] = sum(cnts[max(0,v-3):v])
 
 print(cnts[-1])
