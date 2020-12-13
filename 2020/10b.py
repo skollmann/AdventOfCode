05e21d26c52cadf33a1f46ec9eed715748f05bc8
Stephan Kollmann <stephan.kollmann--git@cl.cam.ac.uk>
2020-12-13 13:52:37 +0100

    Add comment to 10b.py

--- 2020/10b.py
+++ 2020/10b.py
@@ -5,9 +5,11 @@ for line in sys.stdin:
     jolts.append(int(line.rstrip()))
 
 jolts.sort()
+
+# cnts[j] = number of arrangements ending with joltage j (0 if no adapter with joltage j exists)
 cnts = [0 for _ in range(jolts[-1]+1)]
 cnts[0] = 1
-for v in jolts:
-    cnts[v] = sum(cnts[max(0,v-3):v])
+for j in jolts:
+    cnts[j] = sum(cnts[max(0,j-3):j])
 
 print(cnts[-1])
